de5c0fc9face39a4b000e7e49272c00a174553aa
litengat <Litengut@icloud.com>
2024-03-10 23:37:13 +0100

    Projektdateien hinzufügen.

---
+++ Deckapp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using CoreAudio;
+using System.Diagnostics;
+
+namespace Deckapp
+{
+    public partial class MainWindow : Window
+    {
+
+        static List<AudioSessionControl2> Sessions = new List<AudioSessionControl2>();
+        public static AudioSessionControl2 selectedSession = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += Main;
+
+
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator(Guid.NewGuid());
+            MMDevice device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            foreach (var session in device.AudioSessionManager2.Sessions)
+            {
+                    if (!session.IsSystemSoundsSession) 
+                    {
+                        Sessions.Add(session);
+                        Process p = Process.GetProcessById((int)session.ProcessID);
+                        testSelect.Items.Add(p.ProcessName);
+                    }
+            }
+            SerialCom serialCom = new SerialCom();
+        }
+
+        private void Main(object sender, EventArgs e)
+        {
+            MasterVolume.Value = selectedSession.AudioMeterInformation.MasterPeakValue;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            selectedSession.SimpleAudioVolume.MasterVolume = (float) testSlider.Value;
+        }
+            
+        private void testSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedSession = Sessions[testSelect.SelectedIndex];
+            testSlider.Value = selectedSession.SimpleAudioVolume.MasterVolume;
+        }
+    }
+}
---
+++ Deckapp/SerialCom.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO.Ports;  
+
+
+namespace Deckapp
+{
+    class SerialCom
+    {
+        SerialPort serialPort;
+        public SerialCom() {
+            Thread serialConection = new Thread(SerialConection);
+            serialConection.Start();
+        }
+        public void SerialConection()
+        {
+            SearchCom();
+
+            string message;
+            Console.WriteLine("te");
+            while (true)
+            {
+                if (serialPort.BytesToRead > 0)
+                {
+
+                    message = serialPort.ReadLine();
+                    string[] subs = message.Split(';');
+                    foreach (string s in subs)
+                    {
+                        if(s.StartsWith("Fader1"))
+                        {
+                            string[] splited = message.Split(':');
+                            Int32.TryParse(splited[1], out int volume);
+                            Console.WriteLine(volume);
+                            Deckapp.MainWindow.selectedSession.SimpleAudioVolume.MasterVolume = volume / 100.0F;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        public bool SearchCom() {
+            foreach (string COM in SerialPort.GetPortNames())
+            {
+                Console.WriteLine($"SearchCom: {COM}");
+                try {
+                    serialPort = new SerialPort(COM, 9600);
+                    serialPort.Open();
+                }
+                catch
+                {
+                    Console.WriteLine("Com Error");
+                }
+                serialPort.WriteLine("Hi");
+                Thread.Sleep(1000);
+                if (serialPort.BytesToRead > 0)
+                {
+                    string message = serialPort.ReadLine();
+                    Console.WriteLine($"Message: {message}");
+                    if (message == "Hi")
+                    {
+                        Console.WriteLine("Device conected");
+                        return true;
+                    }
+                }
+                //serialPort.Close();
+                Console.WriteLine("Device is not a Deck");
+                return false;
+            }
+            return false;
+        }
+    }
+}
